615e7e562036a8cbb1d237528cf4a709a5809119
Mabelys Arias <MabelysA@gmail.com>
2021-09-24 12:39:27 -0400

    creates sessions controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,8 @@ Rails.application.routes.draw do
   post "/genres" => "genres#create"
   patch "/genres/:id" => "genres#update"
   delete "/genres/:id" => "genres#destroy"
+
+  ## Sessions
+
+  post "/sessions" => "sessions#create"
 end

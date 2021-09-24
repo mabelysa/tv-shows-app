35de022f5461e2118b88eb8df8ab8b6392f97068
Mabelys Arias <MabelysA@gmail.com>
2021-09-24 14:06:39 -0400

    adds model associations to all serializers

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,3 +1,9 @@
 class ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :description, :creator, :seasons, :network, :favorite, :image, :user_id
+
+  has_many :show_genres
+  has_many :genres, through: :show_genres
+  belongs_to :user
+  has_many :actor_shows
+  has_many :actors, through: :actor_shows
 end
